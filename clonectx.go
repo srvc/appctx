9a54b13972358afdecebc56cee7c58443797ce6b
izumin5210 <m@izum.in>
2019-08-09 09:36:54 +0900

    Implement

---
+++ clonectx.go
@@ -0,0 +1,25 @@
+package clonectx
+
+import (
+	"context"
+)
+
+// Clone creates a new Context object but inherits values from the base context object.
+func Clone(base context.Context) context.Context {
+	return &clonedCtx{
+		Context: context.Background(),
+		base:    base,
+	}
+}
+
+type clonedCtx struct {
+	context.Context
+	base context.Context
+}
+
+func (c *clonedCtx) Value(key interface{}) interface{} {
+	if v := c.Context.Value(key); v != nil {
+		return v
+	}
+	return c.base.Value(key)
+}
