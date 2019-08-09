9b74f720caf94523db090fe63f1d7445420ba229
izumin5210 <m@izum.in>
2019-08-09 09:45:02 +0900

    Add test

---
+++ clonectx_test.go
@@ -0,0 +1,31 @@
+package clonectx_test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/izumin5210/clonectx"
+)
+
+func ExampleClone() {
+	debug := func(ctx context.Context) {
+		fmt.Println(
+			"user_id: ", ctx.Value("user_id"), "\t",
+			"request_id: ", ctx.Value("request_id"), "\t",
+			"error: ", ctx.Err(),
+		)
+	}
+
+	baseCtx, cancel := context.WithCancel(context.Background())
+	baseCtx = context.WithValue(baseCtx, "user_id", 123)
+	baseCtx = context.WithValue(baseCtx, "request_id", "7de4f345-8a68-47bc-a1df-90299f95d753")
+
+	newCtx := clonectx.Clone(baseCtx)
+	cancel()
+
+	// Output:
+	// user_id:  123 	 request_id:  7de4f345-8a68-47bc-a1df-90299f95d753 	 error:  context canceled
+	// user_id:  123 	 request_id:  7de4f345-8a68-47bc-a1df-90299f95d753 	 error:  <nil>
+	debug(baseCtx)
+	debug(newCtx)
+}
